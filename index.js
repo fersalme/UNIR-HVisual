150a0e1f915b7bbde49b9d9ced58e163d535b497
Fernando Salcedo <fsalcedo@alzakfoundation.org>
2024-05-01 10:44:40 -0500

    Init

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log("Hola mundo!")
+console.log(5+5)
+function saludo(){
+    console.log("Hola mundo desde una función")
+}
+function sumar (a, b) {
+    console.log("La suma es: ", a+b)
+}
+
+sumar(3, 2)
